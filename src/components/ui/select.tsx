4b733d9233a4587ce9f90723adb3144636d6de25
kmja <karl.mj.andersson@gmail.com>
2026-05-18 19:23:48 +0200

    Add files via upload

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -151,9 +151,9 @@ const SelectContent = React.forwardRef<
             <SelectScrollUpButton />
             <SelectPrimitive.Viewport
                 className={cn(
-                    "p-1",
+                    "p-1 max-h-[300px] overflow-y-auto overscroll-contain",
                     position === "popper" &&
-                        "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
+                        "w-full min-w-[var(--radix-select-trigger-width)]",
                 )}
             >
                 {children}
